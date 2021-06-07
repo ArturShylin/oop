e476659dbe23205238c37b86e23835b4d3559fe8
ArturShylin <85435792+ArturShylin@users.noreply.github.com>
2021-06-07 11:50:43 +0300

    Add files via upload

---
+++ OOP_lab_2/OOP_lab_3/OOP_lab_3/Dyhotomia.cpp
@@ -0,0 +1,42 @@
+//
+// Created by artur on 07-June-21.
+//
+
+#include "Dyhotomia.h"
+#include <cmath>
+#include <iostream>
+
+void Dyhotomia::setBounds(double a, double b) {
+    _a = a;
+    _b = b;
+}
+
+void Dyhotomia::setTolerance(double eps) {
+    _eps = eps;
+}
+
+double Dyhotomia::calculate() {
+    double c = (_a + _b) / 2.0;
+    if (getEquationResult(_a) * getEquationResult(c) < 0) {
+        _b = c;
+    } else {
+        _a = c;
+    }
+
+    if(fabs(_b-_a)<=_eps){
+        return (_a+_b)/2.0;
+    }
+    return calculate();
+}
+
+double Dyhotomia::calculate(double a, double b, double eps) {
+    setBounds(a, b);
+    setTolerance(eps);
+    calculate();
+}
+
+double Dyhotomia::getEquationResult(double x) {
+    std::cout<<3 * sin(sqrt(x)) + 0.35 * x - 3.8<<std::endl;
+    return 3 * sin(sqrt(x)) + 0.35 * x - 3.8;
+
+}
